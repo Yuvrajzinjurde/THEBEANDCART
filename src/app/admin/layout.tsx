53aec3d3fc1cbfceb249b0c1142acf8f5df93770
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-14 20:50:29 +0000

    Improve this design  also reduce the gap between main content are and si

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -11,7 +11,7 @@ export default function AdminLayout({
   return (
     <div className="flex min-h-screen w-full bg-muted/40">
       <AdminSidebar />
-      <main className="flex-1 flex flex-col sm:gap-4 sm:py-4 sm:pl-16">
+      <main className="flex-1 flex flex-col sm:py-4 sm:pl-16">
         {/* You can add a header here if needed, for things like breadcrumbs or page titles */}
         <div className="flex-1 p-4 sm:px-6 sm:py-0">
             {children}
