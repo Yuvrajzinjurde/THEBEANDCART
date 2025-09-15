d1766aa482126d5242faead4c2154eacf49b89f1
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-15 18:44:41 +0000

    this wont be sticky

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 
 import Link from "next/link";
@@ -87,7 +86,7 @@ export default function Header() {
   );
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="w-full border-b bg-background">
       <div className="container flex h-16 items-center px-4 sm:px-6 lg:px-8">
         <Link href={`/${brandName}/home`} className="mr-4 flex items-center space-x-2">
           {brand?.logoUrl ? (
