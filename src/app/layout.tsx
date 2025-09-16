ae77756322b5fcc9cfe5fd029cb618097aaef6aa
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-16 05:15:11 +0000

    change this font of

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
         <ThemeInjector brandName={brandNameFromPath} />
       </head>
       <body className="flex min-h-screen flex-col font-body antialiased">
