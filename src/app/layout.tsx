5e06aae78365c4dc28f3c8f77c033b40eaf23ea4
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-16 05:15:50 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,9 +97,11 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+        <style>
+          {`
+            @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap');
+          `}
+        </style>
         <ThemeInjector brandName={brandNameFromPath} />
       </head>
       <body className="flex min-h-screen flex-col font-body antialiased">
