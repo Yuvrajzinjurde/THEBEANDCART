dfceb8262a48d21808f553759e1c2521b683f7c4
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-20 21:24:58 +0000

    we need to change the font of headings all over the application

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Roboto', 'sans-serif'],
-        headline: ['Roboto', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
