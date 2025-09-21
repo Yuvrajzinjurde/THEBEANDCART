372fef4ec97c930bdddc819178aa3b49f74b0b9a
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-21 10:19:45 +0000

    just for this section in the cart  give 10 centimeters of margin from le

--- src/app/[brand]/cart/page.tsx
+++ src/app/[brand]/cart/page.tsx
@@ -401,7 +401,7 @@ export default function CartPage() {
 
       <main className="container pb-8 pt-6 px-10">
       
-      <div className="mx-auto">
+      <div className="mx-auto" style={{ margin: '0 10cm' }}>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-4">
           <div className="lg:col-span-2">
               <Card>
