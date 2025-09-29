a56ad16335afacc60feaf74202a54cb116ed6073
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-09-29 16:47:19 +0000

    reduce the height of this promotional banner by 30 %

--- src/app/[brand]/home/page.tsx
+++ src/app/[brand]/home/page.tsx
@@ -230,7 +230,7 @@ const PromoBannerSection = ({ brand, brandName }: { brand: IBrand | null, brandN
     const { title, description, imageUrl, imageHint, buttonText, buttonLink } = brand.promoBanner;
     return (
         <section className="container py-12 px-4 sm:px-6 lg:px-8">
-            <div className="relative rounded-2xl overflow-hidden shadow-xl aspect-video md:aspect-[2.5/1] lg:aspect-[3/1]">
+            <div className="relative rounded-2xl overflow-hidden shadow-xl aspect-video md:aspect-[3/1] lg:aspect-[4/1]">
                  <Image
                     src={imageUrl}
                     alt={title}
