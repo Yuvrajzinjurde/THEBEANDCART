1021ad4a196b08308ae02ffa604109ffb90cd05a
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-10-12 19:33:48 +0000

    look at this application how beautifully its header and content es place

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -57,11 +57,11 @@ export default function AdminLayout({
   }, []);
 
   return (
-    <div className="flex w-full bg-muted/40 min-h-screen">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <AdminSidebar />
-      <div className="flex-1 flex flex-col">
+      <div className="flex flex-col">
         <MobileAdminHeader />
-        <main className="flex-1 p-4 sm:p-6">
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 bg-muted/40">
             {children}
         </main>
       </div>
