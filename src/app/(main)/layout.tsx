251f7b24145ea2fb1a76a1e5e2a8893c9029b9dd
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-10-14 17:45:45 +0000

    Error: Could not find the module "[project]/src/components/header.tsx#de

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,22 @@
 
+"use client";
+
+import Header from "@/components/header";
+import { GlobalFooter } from "@/components/global-footer";
+
 export default function MainSiteLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+
+  return (
+    <>
+        <Header />
+        <div className="flex-1 w-full flex flex-col">
+            {children}
+        </div>
+        <GlobalFooter />
+    </>
+  );
 }
