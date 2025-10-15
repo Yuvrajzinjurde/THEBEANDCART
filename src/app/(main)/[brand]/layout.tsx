d7df8f3fdbceaf5d998d40f569f83141fddc513e
Yuvraj Zinjurde <yuvrajxt@gmail.com>
2025-10-15 21:08:44 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '..

--- src/app/(main)/[brand]/layout.tsx
+++ src/app/(main)/[brand]/layout.tsx
@@ -1,17 +1,9 @@
-
-
 'use client';
-
-import AppShell from "../(app)/shell";
 
 export default function BrandLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <AppShell>
-      {children}
-    </AppShell>
-  );
+  return <>{children}</>;
 }
